6dd28e46eb08549e6bf91d9aeae4a749ed816187
Zhaoqing-wang <851091628@qq.com>
2025-07-19 18:58:11 +0800

    20250719 1900 commit

--- R/onattach.R
+++ R/onattach.R
@@ -1,15 +1,17 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   if (requireNamespace("crayon", quietly = TRUE)) {
-    message(paste(
+    message <- paste(
       "Please cite: Wang Z (2025). ",
       crayon::italic("SlimR: Marker-Based Package for Single-Cell and ST Annotation"),
       ". R package version ", crayon::bold("v1.0.2"),
-      ". Available at: ", crayon::url("https://github.com/Zhaoqing-wang/SlimR")
-    ))
+      ". Available at: https://github.com/Zhaoqing-wang/SlimR"
+    )
   } else {
-    message(paste(
+    message <- paste(
       "Please cite: Wang Z (2025). SlimR: Marker-Based Package for Single-Cell and ST Annotation. ",
-      "R package version v1.0.2 Available at: https://github.com/Zhaoqing-wang/SlimR"
-    ))
+      "R package version v1.0.2. Available at: https://github.com/Zhaoqing-wang/SlimR"
+    )
   }
+
+  packageStartupMessage(message)
 }
