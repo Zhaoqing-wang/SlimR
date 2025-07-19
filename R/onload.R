a1d758a46deb0f33cf3381ba45fdc5b7e94b62c5
Zhaoqing-wang <851091628@qq.com>
2025-07-19 18:28:11 +0800

    20250719 1825 commit

--- R/onload.R
+++ R/onload.R
@@ -1,9 +1,15 @@
 .onLoad <- function(libname, pkgname) {
   if (requireNamespace("crayon", quietly = TRUE)) {
-    message(paste("Please cite: Wang Z (2025). ",
-                  crayon::italic("SlimR: Marker-Based Package for Single-Cell and ST Annotation"),
-                  ". R package version 1.0.2"))
+    message(paste(
+      "Please cite: Wang Z (2025). ",
+      crayon::italic("SlimR: Marker-Based Package for Single-Cell and ST Annotation"),
+      ". R package version ", crayon::bold("v1.0.2"),
+      ". Available at: ", crayon::url("https://github.com/Zhaoqing-wang/SlimR")
+    ))
   } else {
-    message("Please cite: Wang Z (2025). SlimR: Marker-Based Package for Single-Cell and ST Annotation. R package version 1.0.2")
+    message(paste(
+      "Please cite: Wang Z (2025). SlimR: Marker-Based Package for Single-Cell and ST Annotation. ",
+      "R package version v1.0.2 Available at: https://github.com/Zhaoqing-wang/SlimR"
+    ))
   }
 }
