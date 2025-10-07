c25e637e07a62cff27217cec820f9a91988f159b
Zhaoqing-wang <851091628@qq.com>
2025-10-07 18:58:07 +0800

    20251007 1900 commit

--- R/onattach.R
+++ R/onattach.R
@@ -2,13 +2,13 @@
   if (requireNamespace("crayon", quietly = TRUE)) {
     message <- paste0(
       "Please cite: Wang Z (2025). ",
-      crayon::italic("SlimR: Machine Learning-Assisted Marker-Based Package for Single-Cell and Spatial Transcriptomics Annotation."),
+      crayon::italic("SlimR: Machine Learning-Assisted, Marker-Based Tool for Single-Cell and Spatial Transcriptomics Annotation."),
       " R package version", crayon::bold(" 1.0.8."),
       " Available at: https://github.com/Zhaoqing-wang/SlimR"
     )
   } else {
     message <- paste0(
-      "Please cite: Wang Z (2025). SlimR: Machine Learning-Assisted Marker-Based Package for Single-Cell and Spatial Transcriptomics Annotation.",
+      "Please cite: Wang Z (2025). SlimR: Machine Learning-Assisted, Marker-Based Tool for Single-Cell and Spatial Transcriptomics Annotation.",
       "R package version 1.0.8. Available at: https://github.com/Zhaoqing-wang/SlimR"
     )
   }
