bf5c022145e067fdc76496a26503c7ce91a49694
Zhaoqing-wang <851091628@qq.com>
2025-11-23 00:53:36 +0800

    20251123 0055 commit

--- R/Database_Markers_list_PCTIT.R
+++ R/Database_Markers_list_PCTIT.R
@@ -4,11 +4,11 @@
 #'
 #' @format A list with 40 tables.
 #'
-#' @details This list is a table of 40 types of pan-cancer tumor-infiltrating T cell markers obtained from the article "Pan-cancer single cell landscapeof tumor-infiltrating T cells".
+#' @details This list is a table of 40 types of pan-cancer tumor-infiltrating T cell (PCTIT) markers obtained from the article "Pan-cancer single cell landscapeof tumor-infiltrating T cells".
 #'     The data source is "https://doi.org/10.1126/science.abe6474",
 #'     and the reference literature is: L. Zheng et al. (2021) <doi:10.1126/science.abe6474>.
 #'
 #' @family Section_0_Database
 #'
 #' @source \doi{10.1126/science.abe6474}
-"Markers_list_PCSCT"
+"Markers_list_PCTIT"
